0aa9b50dd4442af54986e0d1e7a01788f73c3de2
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-14 08:56:06 -0300

    chore: add getter and setter in notification entity

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -2,6 +2,8 @@ export interface NotificationProps {
   content: string;
   category: string;
   readAt?: Date | null;
+  createdAt: Date;
+  recipientId: string;
 }
 
 export class Notification {
@@ -17,5 +19,33 @@ export class Notification {
 
   public get content(): string {
     return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
   }
 }
